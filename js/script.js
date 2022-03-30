cb43b054edd8e88a7064e940ab3e25e8479eec4c
Adam Lewandowski <lewandowskiadam6@gmnail.com>
2022-03-30 23:20:23 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -9,50 +9,66 @@
 
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+    
+        render();
+    };
+    
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+                    render();
+    }
+    
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li>
+                <button class="js-remove">usuń</button>
                 ${task.content}
             </li>
            `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+              removeTask(index);  
+            });
+    });
     };
 
-    const addNewTask = (newTaskContent) =>{
-        tasks.push({
-            content: newTaskContent,
-        });
 
-        render();
-    };
+render();
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+        return;
+
+    }
+
+    addNewTask(newTaskContent);
+
+};
+
+
+const init = () => {
+    render();
     
-    const onFormSubmit = (event) => {
-            event.preventDefault();
+    const form = document.querySelector(".js-form");
 
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    form.addEventListener("submit", onFormSubmit);
+};
 
-            if (newTaskContent === "") {
-                return;
-
-            }
-            
-            addNewTask(newTaskContent);
-            
-        };
-    
-
-    const init = () => {
-        render();
-
-        const form = document.querySelector(".js-form");
-
-        form.addEventListener("submit", onFormSubmit);
-    };
-
-    init();
+init();
 }
